b58c2929134a2e03bb57a78f6e651e593d7ab8b9
Madhavan Mi <80858743+miuiprojects@users.noreply.github.com>
2022-03-13 07:36:31 +0530

    Update help.py

--- gdrive/plugins/help.py
+++ gdrive/plugins/help.py
@@ -43,7 +43,7 @@ def map(pos):
         return [
             [
              InlineKeyboardButton(text = 'Support Chat', url = SUPPORT_CHAT_LINK),
-             InlineKeyboardButton(text = 'Channel Update', url = "t.me/IDNCoder")
+             InlineKeyboardButton(text = 'Channel Update', url = "t.me/@madhavanmi")
             ],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
 
